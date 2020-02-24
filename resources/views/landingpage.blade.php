467a0eb07e642252faf9a6dfad9a40fe329ae7a7
Lino Muñoz <55246628+linomu@users.noreply.github.com>
2020-02-24 14:15:21 -0500

    Add files via upload

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -60,11 +60,11 @@
               <div class="d-flex ml-auto">
                 <a href="#" class="d-flex align-items-center ml-auto mr-4">
                   <span class="icon-envelope mr-2"></span>
-                  <span class="d-none d-md-inline-block">info@domain.com</span>
+                  <span class="d-none d-md-inline-block">linomu@unicauca.edu.co</span>
                 </a>
                 <a href="#" class="d-flex align-items-center">
                   <span class="icon-phone mr-2"></span>
-                  <span class="d-none d-md-inline-block">+1 234 4567 8910</span>
+                  <span class="d-none d-md-inline-block">+57 3218598601</span>
                 </a>
               </div>
             </div>
@@ -91,15 +91,10 @@
                       <a href="#about-section" class="nav-link">Acerca de nosotros</a>
                       <ul class="dropdown arrow-top">
                         <li><a href="#our-team-section" class="nav-link">Equipo</a></li>
-                        
-                        
                       </ul>
                     </li>
-                    <li>
-                      <a href="#projects-section" class="nav-link">Projects</a>
-                    </li>
                     
-                    <li><a href="#services-section" class="nav-link">Services</a></li>
+                    <li><a href="#services-section" class="nav-link">Servicios</a></li>
                     <li><a href="#pricing-section" class="nav-link">Acciones</a></li>
                     <li><a href="#faq-section" class="nav-link">FAQ</a></li>
                     @if (Route::has('login'))
@@ -107,10 +102,10 @@
                     @auth
                         <li><a href="{{ url('/admin') }}" class="nav-link">Dashboard</a></li>
                     @else
-                        <li><a href="{{ route('login') }}" class="nav-link">Login</a></li>
+                        <li><a href="{{ route('login') }}" class="nav-link">Ingresar</a></li>
 
                         @if (Route::has('register'))
-                            <li><a href="{{ route('register') }}" class="nav-link">Register</a></li>
+                            <li><a href="{{ route('register') }}" class="nav-link">Registrarse</a></li>
                         @endif
                     @endauth
                 
@@ -130,7 +125,7 @@
           <div class="col-md-8">
             
             <h1 class="text-uppercase">Satisfacción de Usuario</h1>
-            <span class="sub-text mb-3 d-block"><em>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam molestiae ipsam, atque.</em></span>
+            <span class="sub-text mb-3 d-block"><em>Creación y realización de encuestas para medir la satisfacción del usuario de una o varias páginas específicas.</em></span>
           </div>
         </div>
       </div>
@@ -147,9 +142,8 @@
             <img src="images/hero_bg_1.jpg" alt="Image" class="img-fluid">
           </div>
           <div class="col-lg-4 ml-auto">
-            <span class="sub-title">Learn To Know</span>
-            <h3 class="mb-4">Acerca de nosotros</h3>
-            <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate fuga ipsum commodi aliquid aspernatur reiciendis enim cum voluptas id itaque, asperiores modi, voluptatibus sed voluptate nulla et ratione aliquam! Quaerat.</p>
+            <h3 class="mb-4" style="color:#ff5733">Acerca de nosotros</h3>
+            <p class="mb-4">Somo un equipo de desarrollo. Estamos formándonos en Ingeniería de Sistemas en la Universidad del Cauca. Nuestro equipo tiene solidas bases en diferentes áreas del desarrollo web, las cuáles aplicamos con su respectivo seguimiento en este sitio web. <br>Somos fuertes en: </p>
             <ul class="list-unstyled ul-check text-left success mb-5">
                 <li>Asperiores modi sed</li>
                 <li>Enim cum voluptas</li>
@@ -191,7 +185,7 @@
               </div>
               <h2>Juan Felipe Muñoz</h2>
               <span class="sub-title d-block mb-3">Front End</span>
-              <p>Decidí estudiar ingeniería porque me gusta administrar sistemas tecnológicos enfocados hacia distintas áreas para encontrar una solución con aplicación en ese área</p>
+              <p>Decidí estudiar ingeniería porque me gusta administrar sistemas tecnológicos enfocados hacia distintas áreas para encontrar una solución con aplicación en esa área</p>
             </div>
           </div>
 
@@ -300,7 +294,7 @@
 
           <div class="col-lg-4 col-md-6 mb-4 mb-lg-0 pricing">
             <div class="border p-5 text-center rounded">
-              <h3>Encuestador</h3>
+              <h3>Evaluador</h3>
               
               <ul class="list-unstyled ul-check text-left success mb-5">
                 <li>Gestionar encuestas</li>
@@ -319,7 +313,7 @@
               
               <ul class="list-unstyled ul-check text-left success mb-5">
                 <li>Gestionar  administradores</li>
-                <li>Gestionar encuestadores</li>
+                <li>Gestionar evaluadores</li>
                 <li>Ver encuestas</li>
                 <li>Ver estadísticas por encuesta</li>
               </ul>
@@ -342,7 +336,7 @@
           </div>
           
           <div class="col-lg-6 ml-auto pl-lg-5">
-            <span class="sub-title">Pregúntanos! Estamos felices de responderte</span>
+            <span class="sub-title">Pregúntanos! Estaremos complacidos de responderte</span>
             <h2 class="font-weight-bold text-black mb-5">Preguntas comunes</h2>
             <div class="accordion" id="accordionExample">
               
@@ -377,7 +371,7 @@
               <div class="accordion-item">
                 <h2 class="mb-0 rounded mb-2">
                   <a href="#" class="collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                    ¿Cuántos campos tendrá mi encuesta?
+                    How it works? Please let us know!
                   </a>
                 </h2>
                 
@@ -426,106 +420,18 @@
    
 
     
-    <div class="site-section" id="projects-section">
-      <div class="container">
-        <div class="row justify-content-center">
-          <div class="col-md-7 text-center">
-            <span class="sub-title">Awesome Projects</span>
-            <h2 class="font-weight-bold text-black">Our Projects</h2>
-            <p class="mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi, explicabo, quasi. Magni deserunt sunt labore.</p>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-lg-4 col-md-6 mb-4 project-entry">
-            <a href="images/img_1.jpg" class="d-block figure" data-fancybox="gallery">
-              <img src="images/img_1.jpg" alt="Image" class="img-fluid">
-            </a>
-            <h3 class="mb-0"><a href="#">Butterfly House</a></h3>
-            <span class="text-muted">Interior</span>
-          </div>
-          <div class="col-lg-4 col-md-6 mb-4 project-entry">
-            <a href="images/img_2.jpg" class="d-block figure" data-fancybox="gallery">
-              <img src="images/img_2.jpg" alt="Image" class="img-fluid">
-            </a>
-            <h3 class="mb-0"><a href="#">Interior Bed Design</a></h3>
-            <span class="text-muted">Design</span>
-          </div>
-          <div class="col-lg-4 col-md-6 mb-4 project-entry">
-            <a href="images/img_3.jpg" class="d-block figure" data-fancybox="gallery">
-              <img src="images/img_3.jpg" alt="Image" class="img-fluid">
-            </a>
-            <h3 class="mb-0"><a href="#">Kitchen Sink Design</a></h3>
-            <span class="text-muted">Interior</span>
-          </div>
-
-          <div class="col-lg-4 col-md-6 mb-4 project-entry">
-            <a href="images/img_4.jpg" class="d-block figure" data-fancybox="gallery">
-              <img src="images/img_4.jpg" alt="Image" class="img-fluid">
-            </a>
-            <h3 class="mb-0"><a href="#">Lobby Interior Design</a></h3>
-            <span class="text-muted">Interior</span>
-          </div>
-          <div class="col-lg-4 col-md-6 mb-4 project-entry">
-            <a href="images/img_5.jpg" class="d-block figure" data-fancybox="gallery">
-              <img src="images/img_5.jpg" alt="Image" class="img-fluid">
-            </a>
-            <h3 class="mb-0"><a href="#">Relaxation Room Design</a></h3>
-            <span class="text-muted">Design</span>
-          </div>
-          <div class="col-lg-4 col-md-6 mb-4 project-entry">
-            <a href="images/img_6.jpg" class="d-block figure" data-fancybox="gallery">
-              <img src="images/img_1.jpg" alt="Image" class="img-fluid">
-            </a>
-            <h3 class="mb-0"><a href="#">Butterfly House</a></h3>
-            <span class="text-muted">Interior</span>
-          </div>
-        </div>
-      </div>
-    </div>
-
+    
 
     
 
-    <form action="#" class="p-5 bg-white">
-
-      <div class="row form-group">
-        <div class="col-md-12 mb-3 mb-md-0">
-          <label class="font-weight-bold" for="fullname">Full Name</label>
-          <input type="text" id="fullname" class="form-control" placeholder="Full Name">
-        </div>
-      </div>
-      <div class="row form-group">
-        <div class="col-md-12">
-          <label class="font-weight-bold" for="email">Email</label>
-          <input type="email" id="email" class="form-control" placeholder="Email Address">
-        </div>
-      </div>
-  
-      
-  
-      <div class="row form-group">
-        <div class="col-md-12">
-          <label class="font-weight-bold" for="message">Message</label> 
-          <textarea name="message" id="message" cols="30" rows="5" class="form-control" placeholder="Say hello to us"></textarea>
-        </div>
-      </div>
-  
-      <div class="row form-group">
-        <div class="col-md-12">
-          <input type="submit" value="Send Message" class="btn btn-primary rounded-0 btn-lg">
-        </div>
-      </div>
-  
-  
-    </form>
-
+ 
     <div class="site-section" id="services-section">
       <div class="container">
         <div class="row justify-content-center">
           <div class="col-md-7 text-center">
-            <span class="sub-title">Services</span>
-            <h2 class="font-weight-bold text-black">Our Services</h2>
-            <p class="mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi, explicabo, quasi. Magni deserunt sunt labore.</p>
+            <span class="sub-title">Servicios</span>
+            <h2 class="font-weight-bold text-black">Nuestros servicios</h2>
+            <p class="mb-5">Nuestra finalidad es brindarle a las personas una manera fácil y sencilla de manejar y/o gestionar encuestas acerca de una o varias páginas específicas.</p>
           </div>
         </div>
 
@@ -541,8 +447,8 @@
             <div class="media custom-media">
               <div class="mr-3 icon"><span class="flaticon-window display-4"></span></div>
               <div class="media-body">
-                <h5 class="mt-0">Modern Window Design</h5>
-                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin..
+                <h5 class="mt-0">Gestionar Usuarios</h5>
+                Permitir a los usuarios registrarse e ingresar en nuestro sistema con sus datos personales básicos para poder realizar encuestas.
               </div>
             </div>
           </div>
@@ -552,8 +458,8 @@
             <div class="media custom-media">
               <div class="mr-3 icon"><span class="flaticon-measuring display-4"></span></div>
               <div class="media-body">
-                <h5 class="mt-0">Experts Architect</h5>
-                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin..
+                <h5 class="mt-0">Gestionar Evaluadores</h5>
+                Permitir a los evaluadores  gestionar encuestas y su perfil para posteriormente poder visualizar estadísticas para cada una de las encuestas creadas. 
               </div>
             </div>
           </div>
@@ -562,8 +468,8 @@
             <div class="media custom-media">
               <div class="mr-3 icon"><span class="flaticon-sit-down display-4"></span></div>
               <div class="media-body">
-                <h5 class="mt-0">Sofa Design</h5>
-                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin..
+                <h5 class="mt-0">Gestionar Administradores</h5>
+                Permitir a los administradores gestionar otros administradores y evaluadores. Además de eso, se permite ver las encuestas y ver sus propias estadísiticas.
               </div>
             </div>
           </div>
@@ -571,6 +477,7 @@
         </div>
       </div>
     </div>
+    
 
     
 
@@ -581,73 +488,22 @@
             <div class="row mb-5">
               <div class="col-12">
                 <h3 class="footer-heading mb-4">Acerca de nosotros</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non rem iusto nisi, eum vitae quas culpa fugiat itaque aliquam nulla, accusamus aspernatur molestias recusandae voluptates ratione dolores deleniti velit. Corporis.</p>
+                <p>Somos estudiantes del programa de Ingeniería de Sistemas de la Universidad del Cauca. Cada uno de nosotros cuenta con diferentes conocimientos y actitudes que han hecho de esta aplicación web una realidad.</p>
               </div>
-            </div>
-            <div class="row mb-5">
-              <div class="col-md-12">
-                <h3 class="footer-heading mb-4">Navigation</h3>
-              </div>
-              <div class="col-md-6 col-lg-6">
-                <ul class="list-unstyled">
-                  <li><a href="#">Home</a></li>
-                  <li><a href="#">Services</a></li>
-                  <li><a href="#">News</a></li>
-                  <li><a href="#">Team</a></li>
-                </ul>
-              </div>
-              <div class="col-md-6 col-lg-6">
-                <ul class="list-unstyled">
-                  <li><a href="#">Interior Design</a></li>
-                  <li><a href="#">Architect</a></li>
-                  <li><a href="#">Privacy</a></li>
-                  <li><a href="#">Membership</a></li>
-                </ul>
-              </div>
-            </div>
-
-            
+            </div>            
           </div>
           <div class="col-lg-4">
            
 
             <div class="mb-5">
-              <h3 class="footer-heading mb-4">Recent News</h3>
-              <div class="block-25">
+              <h3 class="footer-heading mb-4">Navegación</h3>
+              <div class="col-md-6 col-lg-6">
                 <ul class="list-unstyled">
-                  <li class="mb-3">
-                    <a href="#" class="d-flex">
-                      <figure class="image mr-4">
-                        <img src="images/img_2.jpg" alt="" class="img-fluid">
-                      </figure>
-                      <div class="text">
-                        <span class="small text-uppercase date">Jan 16, 2018</span>
-                        <h3 class="heading font-weight-light">Lorem ipsum dolor sit amet consectetur elit</h3>
-                      </div>
-                    </a>
-                  </li>
-                  <li class="mb-3">
-                    <a href="#" class="d-flex">
-                      <figure class="image mr-4">
-                        <img src="images/img_3.jpg" alt="" class="img-fluid">
-                      </figure>
-                      <div class="text">
-                        <span class="small text-uppercase date">Jan 16, 2018</span>
-                        <h3 class="heading font-weight-light">Lorem ipsum dolor sit amet consectetur elit</h3>
-                      </div>
-                    </a>
-                  </li>
-                  <li class="mb-3">
-                    <a href="#" class="d-flex">
-                      <figure class="image mr-4">
-                        <img src="images/img_4.jpg" alt="" class="img-fluid">
-                      </figure>
-                      <div class="text">
-                        <span class="small text-uppercase date">Jan 16, 2018</span>
-                        <h3 class="heading font-weight-light">Lorem ipsum dolor sit amet consectetur elit</h3>
-                      </div>
-                    </a>
-                  </li>
+                  <li><a href="#home-section">Inicio</a></li>
+                  <li><a href="#about-section">Acerca de nosotros</a></li>
+                  <li><a href="#services-section">Servicios</a></li>
+                  <li><a href="#pricing-section">Acciones</a></li>
+                  <li><a href="#faq-section">FAQ</a></li>
                 </ul>
               </div>
             </div>
@@ -657,24 +513,8 @@
 
           <div class="col-lg-4 mb-5 mb-lg-0">
 
-            <div class="mb-5">
-              <h3 class="footer-heading mb-2">Subscribe Newsletter</h3>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit minima minus odio.</p>
-
-              <form action="#" method="post" class="form-subscribe">
-                <div class="input-group mb-3">
-                  <input type="text" class="form-control border-white text-white bg-transparent" placeholder="Enter Email" aria-label="Enter Email" aria-describedby="button-addon2">
-                  <div class="input-group-append">
-                    <button class="btn btn-primary" type="button" id="button-addon2">Send</button>
-                  </div>
-                </div>
-              </form>
-
-            </div>
-
-            <div class="row">
-              <div class="col-md-12">
-                <h3 class="footer-heading mb-4">Follow Us</h3>
+          <div class="col-md-12">
+                <h3 class="footer-heading mb-4">Síguenos en</h3>
 
                 <div>
                   <a href="#" class="pl-0 pr-3"><span class="icon-facebook"></span></a>
@@ -683,6 +523,9 @@
                   <a href="#" class="pl-3 pr-3"><span class="icon-linkedin"></span></a>
                 </div>
               </div>
+
+            <div class="row">
+              
             </div>
 
 
