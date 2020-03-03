adcbedbde0c35622ea576465a721695cacec4c4e
Lino Muñoz <55246628+linomu@users.noreply.github.com>
2020-03-02 23:01:37 -0500

    Update 2020_02_29_204022_create_evaluators_table.php

--- database/migrations/2020_02_29_204022_create_evaluators_table.php
+++ database/migrations/2020_02_29_204022_create_evaluators_table.php
@@ -14,8 +14,8 @@ class CreateEvaluatorsTable extends Migration
     public function up()
     {
         Schema::create('evaluators', function (Blueprint $table) {
-            $table->integer('id')->primary('id');
-            $table->integer('admins_id')->unique()->nullable();
+             $table->integer('id')->primary('id');
+            $table->integer('admins_id')->nullable();
             $table->foreign('admins_id')->references('id')->on('admins');;
             $table->string('username',40);
             $table->string('password',250);
