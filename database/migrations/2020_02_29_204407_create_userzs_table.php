1e6763e00e5438eae1a0c5300e43628be9038945
Lino Muñoz <55246628+linomu@users.noreply.github.com>
2020-03-02 23:02:11 -0500

    Update 2020_02_29_204407_create_userzs_table.php

--- database/migrations/2020_02_29_204407_create_userzs_table.php
+++ database/migrations/2020_02_29_204407_create_userzs_table.php
@@ -15,7 +15,7 @@ class CreateUserzsTable extends Migration
     {
         Schema::create('userzs', function (Blueprint $table) {
           $table->increments('id');
-            $table->unsignedInteger('localizations_id')->unique()->nullable();
+            $table->unsignedInteger('localizations_id')->nullable();
             $table->foreign('localizations_id','fk_userzs__localizations')->references('id')->on('localizations');
             $table->string('email',100);
             $table->timestamps(); 
