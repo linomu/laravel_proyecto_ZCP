74c4a2340e5de23d0f77cf4bde51a5dddb8d61c9
Tomas escobar <tomasj@unicauca.edu.co>
2020-03-02 20:52:13 -0500

    Las migraciones se actualizaron Diagrama 1.6.2

--- database/migrations/2020_02_29_203638_create_admins_table.php
+++ database/migrations/2020_02_29_203638_create_admins_table.php
@@ -14,8 +14,7 @@ class CreateAdminsTable extends Migration
     public function up()
     {
         Schema::create('admins', function (Blueprint $table) {
-            $table->Integer('id');
-            $table->primary('id');
+            $table->Integer('id')->primary();
             $table->string('username',40);
             $table->string('password',250);
             $table->string('email',150);
