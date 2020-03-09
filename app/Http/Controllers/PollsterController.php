0ce4aed020da000752a9ca02eaf5507e19ae0e2e
lino <linomu@unicauca.edu.co>
2020-03-08 22:21:38 -0500

    Pollster controller created

--- app/Http/Controllers/PollsterController.php
+++ app/Http/Controllers/PollsterController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App;
 
 class PollsterController extends Controller
 {
@@ -15,7 +16,7 @@ class PollsterController extends Controller
 
     public function index()
     {
-        //
+      
     }
 
 
