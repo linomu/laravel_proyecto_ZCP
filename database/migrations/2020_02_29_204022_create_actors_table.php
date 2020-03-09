a45c8c0814c59d6378a6ba0f376531d8d53ffeba
Tomas escobar <tomasj@unicauca.edu.co>
2020-03-09 18:42:39 -0500

    Falta la reflexion de la tabla actors DE 3.0

---
+++ database/migrations/2020_02_29_204022_create_actors_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateActorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('actors', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->string('firstname',70);
+            $table->string('lastname',70);
+            $table->enum('gender', ['f', 'm']);
+            $table->string('phonenumber',20);
+            $table->date('birth_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('actors');
+    }
+}
